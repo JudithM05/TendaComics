75cb0339f10367046adc64f70db869368d975b71
Maria Manzano Roldán <2023_maria.manzano@iticbcn.cat>
2024-11-22 18:11:43 +0100

    Pantalla d'inici funcional

--- app/src/main/java/com/example/tendacomics/MainActivity.java
+++ app/src/main/java/com/example/tendacomics/MainActivity.java
@@ -1,15 +1,52 @@
 package com.example.tendacomics;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+    private Button Mangues;
+    private Button Comics;
+    private Button Figures;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Mangues = findViewById(R.id.buttonMangues);
+        Comics = findViewById(R.id.buttonComics);
+        Figures = findViewById(R.id.buttonFigures);
+
+        Mangues.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BuscarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Comics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BuscarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Figures.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BuscarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
 
         // Solo agregamos el fragmento una vez cuando la actividad se crea por primera vez
         if (savedInstanceState == null) {

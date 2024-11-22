c1084054ee9b6433b0ee19836db120e2e18b1c46
Maria Manzano Roldán <2023_maria.manzano@iticbcn.cat>
2024-11-22 17:27:26 +0100

    Solucionar pagina perfil

--- app/src/main/java/com/example/tendacomics/Perfil.java
+++ app/src/main/java/com/example/tendacomics/Perfil.java
@@ -1,28 +1,29 @@
 package com.example.tendacomics;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
 
 public class Perfil extends Fragment {
-
-    public Perfil() {
-        // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        ImageView cardViewPerfil = rootView.findViewById(R.id.Perfil);
+        cardViewPerfil.setOnClickListener(v -> {
+            if (getActivity() != null) {
+                Intent intent = new Intent(getActivity(), perfilPagina.class);
+                startActivity(intent);
+            }
+        });
+
+        return rootView;
     }
 }
